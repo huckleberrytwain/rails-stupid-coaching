385055a20e5163b7bdc48825f366ccca9229a051
Lauren Emms <lauren.emms@live.co.uk>
2018-04-02 13:48:37 +0800

    speak

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ActionController::Base
+
+  ANSWERS = {
+    1 => "Great",
+    2 => "Silly question, get dressed and go to work!",
+    3 => "I don't care, get dressed and go to work!"
+  }
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = ANSWERS[1]
+    elsif @question[-1] == '?'
+      @answer = ANSWERS[2]
+    else
+      @answer = ANSWERS[3]
+    end
+  end
+
+end
